6f4add27f993db121c70b23bbcac049b62d58546
=Mtg_Dev <=mtg0987654321@gmail.com>
2021-11-13 19:13:06 +0200

    basic api

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,37 @@
-export async function getServers() {}
+import { socket } from "./socket";
 
-export async function askJoinServer(serverName, password = "") {
-  try {
-    // Connect to Real-Time Channel
-    return true;
-  } catch (error) {
-    // Password is wrong, or server full
-    return false;
-  }
+export async function getServers() {
+  return new Promise((res, rej) => {
+    socket.emit("get-servers", null, (err, servers) => {
+      if (err) return rej(err);
+      res(servers);
+    });
+  });
+}
+
+export async function createServer(serverName, serverPassword = "") {
+  return new Promise((res, rej) => {
+    socket.emit(
+      "create-server",
+      { serverName, serverPassword },
+      (err, serverId) => {
+        if (err) return rej(err);
+        res(serverId);
+      }
+    );
+  });
+}
+
+export async function joinServer(serverId, serverPassword = "") {
+  return new Promise((res, rej) => {
+    const player = JSON.parse(localStorage.getItem("player"));
+    socket.emit(
+      "join-server",
+      { serverId, serverPassword, player },
+      (err, playerId) => {
+        if (err) return rej(err);
+        res(playerId);
+      }
+    );
+  });
 }
